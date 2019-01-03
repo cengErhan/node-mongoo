c7e9b5b0d77cb6ef31f7b4749c45776b6587350e
cengErhan <erhanjava@gmail.com>
2019-01-03 04:29:47 +0300

    Update expressMongoDB.js

--- deneme-MONGODB/expressMongoDB.js
+++ deneme-MONGODB/expressMongoDB.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose')
 var Customer = require('./customer')
 
-mongoose.connect('mongodb://admin:werty253@ds113445.mlab.com:13445/mongoodb',(error)=>{
+mongoose.connect('your_connect_url',(error)=>{
     if (!error) {
         console.log(' CONNECTED = TRUE ')
     }
 })
 
 var customer1 = new Customer({
-    name: "Ehmed Çawlaki",
-    city: "Paupur"
+    name: "name1",
+    city: "city1"
 })
 
 customer1.save((error)=>{
@@ -21,7 +21,7 @@ customer1.save((error)=>{
 })
 
 //boş obje bütün nesneleri çeker
-Customer.find({city:'Paupur'},(error,data)=>{
+Customer.find({city:'city1'},(error,data)=>{
     if(!error){
         console.log(data)
     } else {
@@ -29,7 +29,7 @@ Customer.find({city:'Paupur'},(error,data)=>{
     }
 })
 
-// Customer.findById('5c2bfa5031a756046896fe98',(error,data)=>{
+// Customer.findById('id1',(error,data)=>{
 //     if(!error){
 //         console.log(data)
 //     } else {
@@ -43,19 +43,19 @@ Customer.find({city:'Paupur'},(error,data)=>{
 //     } else {
 //         console.log(data);
 //     }
-// }).where('city').equals('istanbul')//.equals ile devam eder 
+// }).where('city').equals('city1')//.equals ile devam eder 
 //gt(22) = 22 den büyük ; lt(30) = 30 dan küçük ; HEAD.select('head') = uyanın sadece select param. getir
 //gte = büyük eşit ; lte = küçük eşit ; gte(15).lte(30) = 15 ile 30 arası
 //HEAD.select('city name') = HEAD kısmı ile belirtilen kayıtların sadece city ve name ini getir
 //.sort('name'). ; '-name' , 
 
 //kaydı guncelleme ÖNCE müşteri bulundu ve onun üstünden işlem yapıldı
-// Customer.findById('5c2bfa5031a756046896fe98',(error,data)=>{
+// Customer.findById('id1',(error,data)=>{
 //     if (error) {
 //         throw error
 //     } else {
 //         console.log(data)
-//         data.name = 'George W. Bush'
+//         data.name = 'name-2123'
 //         data.save((error)=>{
 //             if (error) {
 //                 throw error
@@ -76,7 +76,7 @@ Customer.find({city:'Paupur'},(error,data)=>{
 // })
 
 // ------------ GELİŞMİŞ SORGULAR ----------------//
-// Customer.findOneAndUpdate({name:'George W. Bush'},{name:'Barrack Obama'},(error,data)=>{
+// Customer.findOneAndUpdate({name:'name1'},{name:'name1111'},(error,data)=>{
 //     if (error) {
 //         throw error
 //     } else {
